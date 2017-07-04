6db0ac4171d90adb66ea4ba4e0975e36831e724c
Robilol <pimous78920@gmail.com>
2017-07-04 12:26:08 +0200

    archi + csv reader

--- main.c
+++ main.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int main()
+{
+    char buffer[1024] ;
+    char *record,*line;
+    int i=0,j=0;
+    char* accountArray[20][10];
+    FILE *fstream = fopen("/home/robilol/Bureau/Projet \C/Bank/Account.csv","r");
+    if(fstream == NULL)
+    {
+        printf("\n file opening failed ");
+        return -1 ;
+    }
+    while((line=fgets(buffer,sizeof(buffer),fstream))!=NULL)
+    {
+        record = strtok(line,";");
+        while(record != NULL)
+        {
+            accountArray[i][j] = record ;
+            record = strtok(NULL,";");
+            j++;
+        }
+        i++ ;
+    }
+    fclose(fstream);
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+
+
+    return 0 ;
 }
